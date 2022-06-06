1a312870897f0fb45b6b5af575b9f3aa4875e480
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-06-06 23:30:48 +0200

    added rollercoastermode toggle

--- src/EDAF80/assignment2.cpp
+++ src/EDAF80/assignment2.cpp
@@ -18,16 +18,16 @@
 #include <cstdlib>
 #include <stdexcept>
 
-edaf80::Assignment2::Assignment2(WindowManager& windowManager) :
-	mCamera(0.5f * glm::half_pi<float>(),
-	        static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
-	        0.01f, 1000.0f),
-	inputHandler(), mWindowManager(windowManager), window(nullptr)
+edaf80::Assignment2::Assignment2(WindowManager &windowManager) : mCamera(0.5f * glm::half_pi<float>(),
+																																				 static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
+																																				 0.01f, 1000.0f),
+																																 inputHandler(), mWindowManager(windowManager), window(nullptr)
 {
-	WindowManager::WindowDatum window_datum{ inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
+	WindowManager::WindowDatum window_datum{inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
 
 	window = mWindowManager.CreateGLFWWindow("EDAF80: Assignment 2", window_datum, config::msaa_rate);
-	if (window == nullptr) {
+	if (window == nullptr)
+	{
 		throw std::runtime_error("Failed to get a window: aborting!");
 	}
 
@@ -39,12 +39,11 @@ edaf80::Assignment2::~Assignment2()
 	bonobo::deinit();
 }
 
-void
-edaf80::Assignment2::run()
+void edaf80::Assignment2::run()
 {
 	// Load the sphere geometry
-	//auto const shape = parametric_shapes::createQuad(0.25f, 0.15f);
-	//auto const shape = parametric_shapes::createSphere(0.15f, 10u, 10u);
+	// auto const shape = parametric_shapes::createQuad(0.25f, 0.15f);
+	// auto const shape = parametric_shapes::createSphere(0.15f, 10u, 10u);
 	auto const shape = parametric_shapes::createTorus(0.2f, 0.05f, 16u, 16u);
 	if (shape.vao == 0u)
 		return;
@@ -58,66 +57,68 @@ edaf80::Assignment2::run()
 	ShaderProgramManager program_manager;
 	GLuint fallback_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Fallback",
-	                                         { { ShaderType::vertex, "common/fallback.vert" },
-	                                           { ShaderType::fragment, "common/fallback.frag" } },
-	                                         fallback_shader);
-	if (fallback_shader == 0u) {
+																					 {{ShaderType::vertex, "common/fallback.vert"},
+																						{ShaderType::fragment, "common/fallback.frag"}},
+																					 fallback_shader);
+	if (fallback_shader == 0u)
+	{
 		LogError("Failed to load fallback shader");
 		return;
 	}
 
 	GLuint diffuse_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Diffuse",
-	                                         { { ShaderType::vertex, "EDAF80/diffuse.vert" },
-	                                           { ShaderType::fragment, "EDAF80/diffuse.frag" } },
-	                                         diffuse_shader);
+																					 {{ShaderType::vertex, "EDAF80/diffuse.vert"},
+																						{ShaderType::fragment, "EDAF80/diffuse.frag"}},
+																					 diffuse_shader);
 	if (diffuse_shader == 0u)
 		LogError("Failed to load diffuse shader");
 
 	GLuint normal_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Normal",
-	                                         { { ShaderType::vertex, "EDAF80/normal.vert" },
-	                                           { ShaderType::fragment, "EDAF80/normal.frag" } },
-	                                         normal_shader);
+																					 {{ShaderType::vertex, "EDAF80/normal.vert"},
+																						{ShaderType::fragment, "EDAF80/normal.frag"}},
+																					 normal_shader);
 	if (normal_shader == 0u)
 		LogError("Failed to load normal shader");
 
 	GLuint tangent_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Tangent",
-	                                         { { ShaderType::vertex, "EDAF80/tangent.vert" },
-	                                           { ShaderType::fragment, "EDAF80/tangent.frag" } },
-	                                         tangent_shader);
+																					 {{ShaderType::vertex, "EDAF80/tangent.vert"},
+																						{ShaderType::fragment, "EDAF80/tangent.frag"}},
+																					 tangent_shader);
 	if (tangent_shader == 0u)
 		LogError("Failed to load tangent shader");
 
 	GLuint binormal_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Bitangent",
-	                                         { { ShaderType::vertex, "EDAF80/binormal.vert" },
-	                                           { ShaderType::fragment, "EDAF80/binormal.frag" } },
-	                                         binormal_shader);
+																					 {{ShaderType::vertex, "EDAF80/binormal.vert"},
+																						{ShaderType::fragment, "EDAF80/binormal.frag"}},
+																					 binormal_shader);
 	if (binormal_shader == 0u)
 		LogError("Failed to load binormal shader");
 
 	GLuint texcoord_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Texture coords",
-	                                         { { ShaderType::vertex, "EDAF80/texcoord.vert" },
-	                                           { ShaderType::fragment, "EDAF80/texcoord.frag" } },
-	                                         texcoord_shader);
+																					 {{ShaderType::vertex, "EDAF80/texcoord.vert"},
+																						{ShaderType::fragment, "EDAF80/texcoord.frag"}},
+																					 texcoord_shader);
 	if (texcoord_shader == 0u)
 		LogError("Failed to load texcoord shader");
 
 	auto const light_position = glm::vec3(-2.0f, 4.0f, 2.0f);
-	auto const set_uniforms = [&light_position](GLuint program){
+	auto const set_uniforms = [&light_position](GLuint program)
+	{
 		glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
 	};
 
 	// Set the default tensions value; it can always be changed at runtime
 	// through the "Scene Controls" window.
-	float catmull_rom_tension = 0.0f;
+	float catmull_rom_tension = 0.5f;
 
 	// Set whether the default interpolation algorithm should be the linear one;
 	// it can always be changed at runtime through the "Scene Controls" window.
-	bool use_linear = true;
+	bool use_linear = false;
 
 	// Set whether to interpolate the position of an object or not; it can
 	// always be changed at runtime through the "Scene Controls" window.
@@ -125,42 +126,38 @@ edaf80::Assignment2::run()
 
 	// Set whether to show the control points or not; it can always be changed
 	// at runtime through the "Scene Controls" window.
-	bool show_control_points = false;
+	bool show_control_points = true;
 
 	auto circle_rings = Node();
 	circle_rings.set_geometry(shape);
-	circle_rings.set_program(&fallback_shader, set_uniforms);
-	TRSTransformf& circle_rings_transform_ref = circle_rings.get_transform();
-
+	circle_rings.set_program(&tangent_shader, set_uniforms);
+	TRSTransformf &circle_rings_transform_ref = circle_rings.get_transform();
 
 	//! \todo Create a tesselated sphere and a tesselated torus
-
 
 	glClearDepthf(1.0f);
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
 
-
 	auto const control_point_sphere = parametric_shapes::createSphere(0.1f, 10u, 10u);
 	std::array<glm::vec3, 9> control_point_locations = {
-		glm::vec3( 0.0f,  0.0f,  0.0f),
-		glm::vec3( 1.0f,  1.8f,  1.0f),
-		glm::vec3( 2.0f,  1.2f,  2.0f),
-		glm::vec3( 3.0f,  3.0f,  3.0f),
-		glm::vec3( 3.0f,  0.0f,  3.0f),
-		glm::vec3(-2.0f, -1.0f,  3.0f),
-		glm::vec3(-3.0f, -3.0f, -3.0f),
-		glm::vec3(-2.0f, -1.2f, -2.0f),
-		glm::vec3(-1.0f, -1.8f, -1.0f)
-	};
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(1.0f, 1.8f, 1.0f),
+			glm::vec3(2.0f, 1.2f, 2.0f),
+			glm::vec3(3.0f, 3.0f, 3.0f),
+			glm::vec3(3.0f, 0.0f, 3.0f),
+			glm::vec3(-2.0f, -1.0f, 3.0f),
+			glm::vec3(-3.0f, -3.0f, -3.0f),
+			glm::vec3(-2.0f, -1.2f, -2.0f),
+			glm::vec3(-1.0f, -1.8f, -1.0f)};
 	std::array<Node, control_point_locations.size()> control_points;
-	for (std::size_t i = 0; i < control_point_locations.size(); ++i) {
-		auto& control_point = control_points[i];
+	for (std::size_t i = 0; i < control_point_locations.size(); ++i)
+	{
+		auto &control_point = control_points[i];
 		control_point.set_geometry(control_point_sphere);
 		control_point.set_program(&diffuse_shader, set_uniforms);
 		control_point.get_transform().SetTranslate(control_point_locations[i]);
 	}
-
 
 	auto lastTime = std::chrono::high_resolution_clock::now();
 
@@ -175,15 +172,17 @@ edaf80::Assignment2::run()
 	bool show_basis = false;
 	float basis_thickness_scale = 1.0f;
 	float basis_length_scale = 1.0f;
+	bool rollerCoasterMode = true;
 
 	changeCullMode(cull_mode);
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		auto const nowTime = std::chrono::high_resolution_clock::now();
 		auto const deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime);
 		lastTime = nowTime;
 
-		auto& io = ImGui::GetIO();
+		auto &io = ImGui::GetIO();
 		inputHandler.SetUICapture(io.WantCaptureMouse, io.WantCaptureKeyboard);
 
 		glfwPollEvents();
@@ -198,7 +197,6 @@ edaf80::Assignment2::run()
 		if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED)
 			mWindowManager.ToggleFullscreenStatusForWindow(window);
 
-
 		// Retrieve the actual framebuffer size: for HiDPI monitors,
 		// you might end up with a framebuffer larger than what you
 		// actually asked for. For example, if you ask for a 1920x1080
@@ -212,22 +210,24 @@ edaf80::Assignment2::run()
 
 		mWindowManager.NewImGuiFrame();
 
-
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 		bonobo::changePolygonMode(polygon_mode);
 
-
-		if (interpolate) {
+		if (interpolate)
+		{
 			static float previousTime = elapsed_time_s;
 			alpha += (elapsed_time_s - previousTime) * speed;
 			previousTime = elapsed_time_s;
 			const auto previousPosition = circle_rings.get_transform().GetTranslation();
-			if (use_linear) {
+			if (use_linear)
+			{
 				const int index0 = static_cast<int>(std::floor(alpha)) % control_point_locations.size();
 				const int index1 = (index0 + 1) % control_point_locations.size();
 				const auto pos = interpolation::evalLERP(control_point_locations[index0], control_point_locations[index1], std::fmod(alpha, 1.f));
 				circle_rings.get_transform().SetTranslate(pos);
-			} else {
+			}
+			else
+			{
 				const int index0 = (static_cast<int>(std::floor(alpha)) + control_point_locations.size() - 1) % control_point_locations.size();
 				const int index1 = (index0 + 1) % control_point_locations.size();
 				const int index2 = (index1 + 1) % control_point_locations.size();
@@ -238,29 +238,38 @@ edaf80::Assignment2::run()
 			circle_rings.get_transform().LookTowards(circle_rings.get_transform().GetTranslation() - previousPosition);
 		}
 
-		mCamera.mWorld.SetTranslate(
-			circle_rings.get_transform().GetTranslation() + circle_rings.get_transform().GetRight() * (0.4f + 0.05f * std::cos(alpha)) + circle_rings.get_transform().GetUp() * (0.4f + 0.05f * std::sin(alpha * 2.17f)) + circle_rings.get_transform().GetBack() * 0.5f);
-		mCamera.mWorld.LookAt(circle_rings.get_transform().GetTranslation() + circle_rings.get_transform().GetFront(), circle_rings.get_transform().GetUp());
+		if (rollerCoasterMode)
+		{
+			mCamera.mWorld.SetTranslate(
+					circle_rings.get_transform().GetTranslation() + circle_rings.get_transform().GetRight() * (0.4f + 0.05f * std::cos(alpha)) + circle_rings.get_transform().GetUp() * (0.4f + 0.05f * std::sin(alpha * 2.17f)) + circle_rings.get_transform().GetBack() * 0.5f);
+			mCamera.mWorld.LookAt(circle_rings.get_transform().GetTranslation() + circle_rings.get_transform().GetFront(), circle_rings.get_transform().GetUp());
+		}
 
 		circle_rings.render(mCamera.GetWorldToClipMatrix());
-		if (show_control_points) {
-			for (auto const& control_point : control_points) {
+		if (show_control_points)
+		{
+			for (auto const &control_point : control_points)
+			{
 				control_point.render(mCamera.GetWorldToClipMatrix());
 			}
 		}
 
 		bool const opened = ImGui::Begin("Scene Controls", nullptr, ImGuiWindowFlags_None);
-		if (opened) {
+		if (opened)
+		{
 			auto const cull_mode_changed = bonobo::uiSelectCullMode("Cull mode", cull_mode);
-			if (cull_mode_changed) {
+			if (cull_mode_changed)
+			{
 				changeCullMode(cull_mode);
 			}
 			bonobo::uiSelectPolygonMode("Polygon mode", polygon_mode);
 			auto selection_result = program_manager.SelectProgram("Shader", program_index);
-			if (selection_result.was_selection_changed) {
+			if (selection_result.was_selection_changed)
+			{
 				circle_rings.set_program(selection_result.program, set_uniforms);
 			}
 			ImGui::Separator();
+			ImGui::Checkbox("Roller Coaster Mode", &rollerCoasterMode);
 			ImGui::Checkbox("Show control points", &show_control_points);
 			ImGui::Checkbox("Enable interpolation", &interpolate);
 			ImGui::Checkbox("Use linear interpolation", &use_linear);
@@ -290,10 +299,13 @@ int main()
 
 	Bonobo framework;
 
-	try {
+	try
+	{
 		edaf80::Assignment2 assignment2(framework.GetWindowManager());
 		assignment2.run();
-	} catch (std::runtime_error const& e) {
+	}
+	catch (std::runtime_error const &e)
+	{
 		LogError(e.what());
 	}
 }
