f84304f4a0d46e7d4d11c5766ca985fed7ff4fbf
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-06-09 21:47:47 +0200

    fix duplicate code

--- src/EDAF80/assignment4.cpp
+++ src/EDAF80/assignment4.cpp
@@ -15,16 +15,16 @@
 #include <clocale>
 #include <stdexcept>
 
-edaf80::Assignment4::Assignment4(WindowManager& windowManager) :
-	mCamera(0.5f * glm::half_pi<float>(),
-	        static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
-	        0.01f, 1000.0f),
-	inputHandler(), mWindowManager(windowManager), window(nullptr)
+edaf80::Assignment4::Assignment4(WindowManager &windowManager) : mCamera(0.5f * glm::half_pi<float>(),
+																																				 static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
+																																				 0.01f, 1000.0f),
+																																 inputHandler(), mWindowManager(windowManager), window(nullptr)
 {
-	WindowManager::WindowDatum window_datum{ inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
+	WindowManager::WindowDatum window_datum{inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
 
 	window = mWindowManager.CreateGLFWWindow("EDAF80: Assignment 4", window_datum, config::msaa_rate, false, true);
-	if (window == nullptr) {
+	if (window == nullptr)
+	{
 		throw std::runtime_error("Failed to get a window: aborting!");
 	}
 
@@ -36,8 +36,7 @@ edaf80::Assignment4::~Assignment4()
 	bonobo::deinit();
 }
 
-void
-edaf80::Assignment4::run()
+void edaf80::Assignment4::run()
 {
 	// Set up the camera
 	mCamera.mWorld.SetTranslate(glm::vec3(-40.0f, 14.0f, 6.0f));
@@ -50,28 +49,30 @@ edaf80::Assignment4::run()
 	ShaderProgramManager program_manager;
 	GLuint fallback_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Fallback",
-	                                         { { ShaderType::vertex, "common/fallback.vert" },
-	                                           { ShaderType::fragment, "common/fallback.frag" } },
-	                                         fallback_shader);
-	if (fallback_shader == 0u) {
+																					 {{ShaderType::vertex, "common/fallback.vert"},
+																						{ShaderType::fragment, "common/fallback.frag"}},
+																					 fallback_shader);
+	if (fallback_shader == 0u)
+	{
 		LogError("Failed to load fallback shader");
 		return;
 	}
 
 	GLuint water_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Water",
-	                                         { { ShaderType::vertex, "EDAF80/water.vert" },
-	                                           { ShaderType::fragment, "EDAF80/water.frag" } },
-	                                         water_shader);
-	if (water_shader == 0u) {
+																					 {{ShaderType::vertex, "EDAF80/water.vert"},
+																						{ShaderType::fragment, "EDAF80/water.frag"}},
+																					 water_shader);
+	if (water_shader == 0u)
+	{
 		LogError("Failed to load water shader");
 		return;
 	}
 	GLuint skybox_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Skybox",
-	                                         { { ShaderType::vertex, "EDAF80/skybox.vert" },
-	                                           { ShaderType::fragment, "EDAF80/skybox.frag" } },
-	                                         skybox_shader);
+																					 {{ShaderType::vertex, "EDAF80/skybox.vert"},
+																						{ShaderType::fragment, "EDAF80/skybox.frag"}},
+																					 skybox_shader);
 	if (skybox_shader == 0u)
 		LogError("Failed to load skybox shader");
 
@@ -81,7 +82,8 @@ edaf80::Assignment4::run()
 	glm::vec3 waterColorDeep(0.f, 0.f, 0.1f);
 	glm::vec3 waterColorShallow(0.f, 0.5f, 0.5f);
 
-	const auto set_water_uniforms = [&](GLuint program) {
+	const auto set_water_uniforms = [&](GLuint program)
+	{
 		glUniform1f(glGetUniformLocation(program, "time"), elapsed_time_s);
 		glUniform3fv(glGetUniformLocation(program, "water_color_deep"), 1, glm::value_ptr(waterColorDeep));
 		glUniform3fv(glGetUniformLocation(program, "water_color_shallow"), 1, glm::value_ptr(waterColorShallow));
@@ -90,14 +92,15 @@ edaf80::Assignment4::run()
 
 	// Setup Skybox
 	const auto my_cube_map_id = bonobo::loadTextureCubeMap(
-		config::resources_path("cubemaps/NissiBeach2/posx.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/negx.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/posy.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/negy.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/posz.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/negz.jpg"));
+			config::resources_path("cubemaps/NissiBeach2/posx.jpg"),
+			config::resources_path("cubemaps/NissiBeach2/negx.jpg"),
+			config::resources_path("cubemaps/NissiBeach2/posy.jpg"),
+			config::resources_path("cubemaps/NissiBeach2/negy.jpg"),
+			config::resources_path("cubemaps/NissiBeach2/posz.jpg"),
+			config::resources_path("cubemaps/NissiBeach2/negz.jpg"));
 	auto skybox_shape = parametric_shapes::createSphere(900.0f, 10u, 10u);
-	if (skybox_shape.vao == 0u) {
+	if (skybox_shape.vao == 0u)
+	{
 		LogError("Failed to retrieve the mesh for the skybox");
 		return;
 	}
@@ -109,7 +112,8 @@ edaf80::Assignment4::run()
 	// Setup Water Plane
 	const auto waves_texture = bonobo::loadTexture2D(config::resources_path("textures/waves.png"));
 	auto plane_shape = parametric_shapes::createQuad(100.f, 100.f, 1000u, 1000u);
-	if (plane_shape.vao == 0u) {
+	if (plane_shape.vao == 0u)
+	{
 		LogError("Failed to retrieve the mesh for the plane");
 		return;
 	}
@@ -118,51 +122,10 @@ edaf80::Assignment4::run()
 	water.set_program(&water_shader, set_water_uniforms);
 	water.add_texture("waves_texture", waves_texture, GL_TEXTURE_2D);
 	water.add_texture("skybox_texture", my_cube_map_id, GL_TEXTURE_CUBE_MAP);
-
-
-	const auto set_water_uniforms = [&](GLuint program) {
-		glUniform1f(glGetUniformLocation(program, "time"), elapsed_time_s);
-		glUniform3fv(glGetUniformLocation(program, "water_color_deep"), 1, glm::value_ptr(waterColorDeep));
-		glUniform3fv(glGetUniformLocation(program, "water_color_shallow"), 1, glm::value_ptr(waterColorShallow));
-		glUniform3fv(glGetUniformLocation(program, "camera_position"), 1, glm::value_ptr(camera_position));
-	};
-
-	// Setup Skybox
-	const auto my_cube_map_id = bonobo::loadTextureCubeMap(
-		config::resources_path("cubemaps/NissiBeach2/posx.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/negx.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/posy.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/negy.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/posz.jpg"),
-		config::resources_path("cubemaps/NissiBeach2/negz.jpg"));
-	auto skybox_shape = parametric_shapes::createSphere(900.0f, 10u, 10u);
-	if (skybox_shape.vao == 0u) {
-		LogError("Failed to retrieve the mesh for the skybox");
-		return;
-	}
-	Node skybox;
-	skybox.set_geometry(skybox_shape);
-	skybox.set_program(&skybox_shader);
-	skybox.add_texture("skybox_texture", my_cube_map_id, GL_TEXTURE_CUBE_MAP);
-
-	// Setup Water Plane
-	const auto waves_texture = bonobo::loadTexture2D(config::resources_path("textures/waves.png"));
-	auto plane_shape = parametric_shapes::createQuad(100.f, 100.f, 1000u, 1000u);
-	if (plane_shape.vao == 0u) {
-		LogError("Failed to retrieve the mesh for the plane");
-		return;
-	}
-	Node water;
-	water.set_geometry(plane_shape);
-	water.set_program(&water_shader, set_water_uniforms);
-	water.add_texture("waves_texture", waves_texture, GL_TEXTURE_2D);
-	water.add_texture("skybox_texture", my_cube_map_id, GL_TEXTURE_CUBE_MAP);
-
 
 	glClearDepth(1.0);
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
-
 
 	auto lastTime = std::chrono::high_resolution_clock::now();
 
@@ -179,32 +142,36 @@ edaf80::Assignment4::run()
 
 	changeCullMode(cull_mode);
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		auto const nowTime = std::chrono::high_resolution_clock::now();
 		auto const deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime);
 		lastTime = nowTime;
-		if (!pause_animation) {
+		if (!pause_animation)
+		{
 			elapsed_time_s += std::chrono::duration<float>(deltaTimeUs).count();
 		}
 
-		auto& io = ImGui::GetIO();
+		auto &io = ImGui::GetIO();
 		inputHandler.SetUICapture(io.WantCaptureMouse, io.WantCaptureKeyboard);
 
 		glfwPollEvents();
 		inputHandler.Advance();
 		mCamera.Update(deltaTimeUs, inputHandler);
-		if (use_orbit_camera) {
+		if (use_orbit_camera)
+		{
 			mCamera.mWorld.LookAt(glm::vec3(0.0f));
 		}
 		camera_position = mCamera.mWorld.GetTranslation();
 
-		if (inputHandler.GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED) {
+		if (inputHandler.GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED)
+		{
 			shader_reload_failed = !program_manager.ReloadAllPrograms();
 			if (shader_reload_failed)
 				tinyfd_notifyPopup("Shader Program Reload Error",
-				                   "An error occurred while reloading shader programs; see the logs for details.\n"
-				                   "Rendering is suspended until the issue is solved. Once fixed, just reload the shaders again.",
-				                   "error");
+													 "An error occurred while reloading shader programs; see the logs for details.\n"
+													 "Rendering is suspended until the issue is solved. Once fixed, just reload the shaders again.",
+													 "error");
 		}
 		if (inputHandler.GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
 			show_logs = !show_logs;
@@ -212,7 +179,6 @@ edaf80::Assignment4::run()
 			show_gui = !show_gui;
 		if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED)
 			mWindowManager.ToggleFullscreenStatusForWindow(window);
-
 
 		// Retrieve the actual framebuffer size: for HiDPI monitors,
 		// you might end up with a framebuffer larger than what you
@@ -225,29 +191,28 @@ edaf80::Assignment4::run()
 		glfwGetFramebufferSize(window, &framebuffer_width, &framebuffer_height);
 		glViewport(0, 0, framebuffer_width, framebuffer_height);
 
-
 		mWindowManager.NewImGuiFrame();
 
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 		bonobo::changePolygonMode(polygon_mode);
 
-
-		if (!shader_reload_failed) {
+		if (!shader_reload_failed)
+		{
 			water.render(mCamera.GetWorldToClipMatrix());
 			skybox.render(mCamera.GetWorldToClipMatrix(), glm::scale(glm::mat4(1.f), glm::vec3(-1.f)));
 		}
 
-
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-
 		bool opened = ImGui::Begin("Scene Control", nullptr, ImGuiWindowFlags_None);
-		if (opened) {
+		if (opened)
+		{
 			ImGui::Checkbox("Pause animation", &pause_animation);
 			ImGui::Checkbox("Use orbit camera", &use_orbit_camera);
 			ImGui::Separator();
 			auto const cull_mode_changed = bonobo::uiSelectCullMode("Cull mode", cull_mode);
-			if (cull_mode_changed) {
+			if (cull_mode_changed)
+			{
 				changeCullMode(cull_mode);
 			}
 			bonobo::uiSelectPolygonMode("Polygon mode", polygon_mode);
@@ -277,10 +242,13 @@ int main()
 
 	Bonobo framework;
 
-	try {
+	try
+	{
 		edaf80::Assignment4 assignment4(framework.GetWindowManager());
 		assignment4.run();
-	} catch (std::runtime_error const& e) {
+	}
+	catch (std::runtime_error const &e)
+	{
 		LogError(e.what());
 	}
 }
