3c0d4b1f55bd03e18837e231184f50d5cea7de4c
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-06-23 23:23:51 +0200

    updated joystick and added crt effect

---
+++ shaders/EDAF80/screen.frag
@@ -0,0 +1,62 @@
+#version 410
+
+uniform sampler2D diffuse_texture;
+uniform int has_diffuse_texture;
+
+uniform float time;
+
+in VS_OUT {
+    vec2 texcoord;
+} fs_in;
+
+out vec4 frag_color;
+
+float hash12(vec2 p) {
+	vec3 p3  = fract(vec3(p.xyx) * .1031);
+    p3 += dot(p3, p3.yzx + 33.33);
+    return fract((p3.x + p3.y) * p3.z);
+}
+float onOff(float a, float b, float c) {
+	return step(c, sin(time + a*cos(time*b)));
+}
+float ramp(float y, float start, float end) {
+	float inside = step(start,y) - step(end,y);
+	float fact = (y-start)/(end-start)*inside;
+	return (1.-fact) * inside;
+}
+float stripes(vec2 uv) {
+	float noi = hash12(uv*vec2(0.5,1.) + vec2(1.,3.));
+	return ramp(mod(uv.y*4. + time/2.+sin(time + sin(time*0.63)),1.),0.5,0.6)*noi;
+}
+vec2 screenDistort(vec2 uv) {
+	uv -= vec2(.5,.5);
+	uv = uv*1.2*(1./1.2+2.*uv.x*uv.x*uv.y*uv.y);
+	uv += vec2(.5,.5);
+	return uv;
+}
+
+void main() {
+    if (has_diffuse_texture != 0) {
+        vec2 uv = fs_in.texcoord;
+        uv = screenDistort(uv);
+
+       	float vigAmt = 3.+.3*sin(time + 5.*cos(time*5.));
+        float vignette = (1.-vigAmt*(uv.y-.5)*(uv.y-.5))*(1.-vigAmt*(uv.x-.5)*(uv.x-.5));
+
+
+        vec2 p = mod(uv * 32.0, vec2(1.0)) * 2.0 - 1.0;
+        float f = pow(1.0-max(abs(p.x),abs(p.y)), 0.4545);
+
+        vec3 col = texture(diffuse_texture, uv).rgb;
+        col *= f;
+        col += stripes(uv) * 0.1;
+        col += hash12(uv*2000.0+time) * 0.2;
+        // col *= vignette;
+        col *= (12.+mod(uv.y*30.+time,1.))/19.;
+
+
+        frag_color = vec4(col, 1.0);
+    } else {
+        frag_color = vec4(1.0);
+    }
+}
--- src/EDAF80/assignment5.cpp
+++ src/EDAF80/assignment5.cpp
@@ -1,30 +1,40 @@
 #include "assignment5.hpp"
 
+#include <clocale>
+#include <stdexcept>
+
+#include <imgui.h>
+#include <tinyfiledialogs.h>
+
 #include "config.hpp"
 #include "core/Bonobo.h"
 #include "core/FPSCamera.h"
 #include "core/ShaderProgramManager.hpp"
 #include "core/helpers.hpp"
 #include "core/node.hpp"
-
-#include <imgui.h>
-#include <tinyfiledialogs.h>
-
+#include "glm/gtc/type_ptr.hpp"
 #include "libtetris.h"
 
-#include <clocale>
-#include <stdexcept>
+static constexpr int RESOLUTION_X = 600;
+static constexpr int RESOLUTION_Y = 1000;
 
-static constexpr uint32_t COLOR_BLACK = 0;
-static constexpr uint32_t COLOR_CYAN = 0xffff00;
-static constexpr uint32_t COLOR_ORANGE = 0x0099ff;
-static constexpr uint32_t COLOR_YELLOW = 0x00ffff;
-static constexpr uint32_t COLOR_RED = 0x0000ff;
+static constexpr int FBW            = 32;
+static constexpr int FBH            = 32;
+static constexpr int BOARD_WIDTH    = 10;
+static constexpr int BOARD_HEIGHT   = 20;
+static constexpr int BOARD_OFFSET_X = 11;
+static constexpr int BOARD_OFFSET_Y = 3;
+
+static constexpr uint32_t COLOR_BLACK   = 0;
+static constexpr uint32_t COLOR_CYAN    = 0xffff00;
+static constexpr uint32_t COLOR_ORANGE  = 0x0099ff;
+static constexpr uint32_t COLOR_YELLOW  = 0x00ffff;
+static constexpr uint32_t COLOR_RED     = 0x0000ff;
 static constexpr uint32_t COLOR_MAGENTA = 0xff00ff;
-static constexpr uint32_t COLOR_BLUE = 0xff0000;
-static constexpr uint32_t COLOR_GREEN = 0x00ff00;
-static constexpr uint32_t COLOR_GHOST = 0x5e5e5e;
-static constexpr uint32_t COLOR_BORDER = 0x333333;
+static constexpr uint32_t COLOR_BLUE    = 0xff0000;
+static constexpr uint32_t COLOR_GREEN   = 0x00ff00;
+static constexpr uint32_t COLOR_GHOST   = 0x5e5e5e;
+static constexpr uint32_t COLOR_BORDER  = 0x333333;
 
 uint32_t get_color(int8_t v) {
   uint32_t color = COLOR_BLACK;
@@ -61,26 +71,12 @@ uint32_t get_color(int8_t v) {
   return color;
 }
 
-static constexpr int RESOLUTION_X = 600;
-static constexpr int RESOLUTION_Y = 1000;
-
-edaf80::Assignment5::Assignment5(WindowManager &windowManager)
-    : mCamera(0.5f * glm::half_pi<float>(),
-              static_cast<float>(RESOLUTION_X) /
-                  static_cast<float>(RESOLUTION_Y),
-              0.01f, 1000.0f),
+edaf80::Assignment5::Assignment5(WindowManager& windowManager)
+    : mCamera(0.5f * glm::half_pi<float>(), static_cast<float>(RESOLUTION_X) / static_cast<float>(RESOLUTION_Y), 0.01f, 1000.0f),
       inputHandler(), mWindowManager(windowManager), window(nullptr) {
-  WindowManager::WindowDatum window_datum{inputHandler,
-                                          mCamera,
-                                          RESOLUTION_X,
-                                          RESOLUTION_Y,
-                                          0,
-                                          0,
-                                          0,
-                                          0};
+  WindowManager::WindowDatum window_datum{inputHandler, mCamera, RESOLUTION_X, RESOLUTION_Y, 0, 0, 0, 0};
 
-  window = mWindowManager.CreateGLFWWindow("EDAF80: Assignment 5", window_datum,
-                                           config::msaa_rate);
+  window = mWindowManager.CreateGLFWWindow("EDAF80: Assignment 5", window_datum, config::msaa_rate);
   if (window == nullptr) {
     throw std::runtime_error("Failed to get a window: aborting!");
   }
@@ -88,50 +84,36 @@ edaf80::Assignment5::Assignment5(WindowManager &windowManager)
   bonobo::init();
 }
 
-edaf80::Assignment5::~Assignment5() { bonobo::deinit(); }
+edaf80::Assignment5::~Assignment5() {
+  bonobo::deinit();
+}
 
 void edaf80::Assignment5::run() {
   // Set up the camera
   mCamera.mWorld.SetTranslate(glm::vec3(0.0f, 2.5f, 7.0f));
   mCamera.mMouseSensitivity = glm::vec2(0.003f);
-  mCamera.mMovementSpeed = glm::vec3(3.0f); // 3 m/s => 10.8 km/h
+  mCamera.mMovementSpeed    = glm::vec3(3.0f); // 3 m/s => 10.8 km/h
 
   // Create the shader programs
   ShaderProgramManager program_manager;
-  GLuint default_shader = 0u;
+  GLuint               default_shader = 0u;
   program_manager.CreateAndRegisterProgram(
-      "Default",
-      {{ShaderType::vertex, "EDAF80/default.vert"},
-       {ShaderType::fragment, "EDAF80/default.frag"}},
-      default_shader);
-  if (default_shader == 0u) {
-    LogError("Failed to load fallback shader");
-    return;
-  }
+      "Default", {{ShaderType::vertex, "EDAF80/default.vert"}, {ShaderType::fragment, "EDAF80/default.frag"}}, default_shader);
+  GLuint screen_shader = 0u;
+  program_manager.CreateAndRegisterProgram(
+      "Screen", {{ShaderType::vertex, "EDAF80/default.vert"}, {ShaderType::fragment, "EDAF80/screen.frag"}}, screen_shader);
 
-  //
-  // Todo: Load your geometry
-  //
-  const auto arcade_machine_meshes = bonobo::loadObjects(
-      config::resources_path("../assets/arcade-machine.fbx"));
-  Node arcade_machine;
+  const auto arcade_machine_meshes = bonobo::loadObjects(config::resources_path("../assets/arcade-machine.fbx"));
+  Node       arcade_machine;
   arcade_machine.set_geometry(arcade_machine_meshes.front());
   arcade_machine.set_program(&default_shader);
 
-  const auto joystick_meshes =
-      bonobo::loadObjects(config::resources_path("../assets/joystick.fbx"));
-  Node joystick;
+  const auto joystick_meshes = bonobo::loadObjects(config::resources_path("../assets/joystick.fbx"));
+  Node       joystick;
   joystick.set_geometry(joystick_meshes.front());
   joystick.set_program(&default_shader);
 
-  static constexpr int FBW = 32;
-  static constexpr int FBH = 32;
-  static constexpr int BOARD_WIDTH = 10;
-  static constexpr int BOARD_HEIGHT = 20;
-  static constexpr int BOARD_OFFSET_X = 11;
-  static constexpr int BOARD_OFFSET_Y = 3;
-
-  tetris_t *tetris_game = create_game();
+  tetris_t*       tetris_game = create_game();
   tetris_inputs_t key_input{};
   init(tetris_game, BOARD_WIDTH, BOARD_HEIGHT, 1000000, 166667, 33333);
 
@@ -139,36 +121,35 @@ void edaf80::Assignment5::run() {
   {
     const auto draw_box = [&](int x, int y, int w, int h) {
       for (int i = y; i < y + h; ++i) {
-        framebuffer[FBH - i][x] = COLOR_BORDER;
+        framebuffer[FBH - i][x]         = COLOR_BORDER;
         framebuffer[FBH - i][x + w - 1] = COLOR_BORDER;
       }
       for (int i = x; i < x + w; ++i) {
-        framebuffer[FBH - y][i] = COLOR_BORDER;
+        framebuffer[FBH - y][i]           = COLOR_BORDER;
         framebuffer[FBH - (y + h - 1)][i] = COLOR_BORDER;
       }
     };
-    draw_box(BOARD_OFFSET_X - 1, BOARD_OFFSET_Y - 1, BOARD_WIDTH + 2,
-             BOARD_HEIGHT + 2);
+    draw_box(BOARD_OFFSET_X - 1, BOARD_OFFSET_Y - 1, BOARD_WIDTH + 2, BOARD_HEIGHT + 2);
     draw_box(BOARD_OFFSET_X - 6, BOARD_OFFSET_Y - 1, 6, 4);
     for (int i = 0; i < 5; ++i) {
       draw_box(BOARD_OFFSET_X + BOARD_WIDTH, BOARD_OFFSET_Y - 1 + i * 4, 6, 4);
     }
   }
 
-  GLuint screen_texture = bonobo::createTexture(
-      FBW, FBH, GL_TEXTURE_2D, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, framebuffer);
-  auto screen_meshes =
-      bonobo::loadObjects(config::resources_path("../assets/screen.fbx"));
-  Node screen;
+  GLuint screen_texture = bonobo::createTexture(FBW, FBH, GL_TEXTURE_2D, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, framebuffer);
+  auto   screen_meshes  = bonobo::loadObjects(config::resources_path("../assets/screen.fbx"));
+  Node   screen;
   screen_meshes[0].bindings["diffuse_texture"] = screen_texture;
   screen.set_geometry(screen_meshes.front());
-  screen.set_program(&default_shader);
+
+  float      elapsed_time_s = 0.0f;
+  const auto set_screen_uniforms = [&](GLuint program) { glUniform1f(glGetUniformLocation(program, "time"), elapsed_time_s); };
+  screen.set_program(&screen_shader, set_screen_uniforms);
 
   GLuint screen_pbo;
   glGenBuffers(1, &screen_pbo);
   glBindBuffer(GL_PIXEL_UNPACK_BUFFER, screen_pbo);
-  glBufferData(GL_PIXEL_UNPACK_BUFFER, FBW * FBH * sizeof(framebuffer[0][0]),
-               framebuffer, GL_STREAM_DRAW);
+  glBufferData(GL_PIXEL_UNPACK_BUFFER, FBW * FBH * sizeof(framebuffer[0][0]), framebuffer, GL_STREAM_DRAW);
   glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 
   glClearDepthf(1.0f);
@@ -177,18 +158,18 @@ void edaf80::Assignment5::run() {
 
   auto lastTime = std::chrono::high_resolution_clock::now();
 
-  bool show_gui = true;
+  bool show_gui             = true;
   bool shader_reload_failed = false;
 
   while (!glfwWindowShouldClose(window)) {
-    auto const nowTime = std::chrono::high_resolution_clock::now();
-    auto const deltaTimeUs =
-        std::chrono::duration_cast<std::chrono::microseconds>(nowTime -
-                                                              lastTime);
-    const auto delta_time = deltaTimeUs.count() / 1000000.0f;
-    lastTime = nowTime;
+    auto const nowTime     = std::chrono::high_resolution_clock::now();
+    auto const deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime);
+    const auto delta_time  = deltaTimeUs.count() / 1000000.0f;
+    lastTime               = nowTime;
 
-    auto &io = ImGui::GetIO();
+    elapsed_time_s += delta_time;
+
+    auto& io = ImGui::GetIO();
     inputHandler.SetUICapture(io.WantCaptureMouse, io.WantCaptureKeyboard);
 
     glfwPollEvents();
@@ -205,26 +186,21 @@ void edaf80::Assignment5::run() {
                            "Once fixed, just reload the shaders again.",
                            "error");
     }
-    if (inputHandler.GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
-      show_gui = !show_gui;
-    if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED)
-      mWindowManager.ToggleFullscreenStatusForWindow(window);
+    if (inputHandler.GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED) show_gui = !show_gui;
+    if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED) mWindowManager.ToggleFullscreenStatusForWindow(window);
 
     if (inputHandler.GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED) {
       set_seed(tetris_game, 0);
       init(tetris_game, 10, 20, 1000000, 166667, 33333);
     }
 
-    key_input.rotate_cw = inputHandler.GetKeycodeState(GLFW_KEY_UP) & PRESSED;
-    key_input.rotate_ccw =
-        inputHandler.GetKeycodeState(GLFW_KEY_LEFT_CONTROL) & PRESSED;
-    key_input.hard_drop =
-        inputHandler.GetKeycodeState(GLFW_KEY_SPACE) & PRESSED;
-    key_input.soft_drop = inputHandler.GetKeycodeState(GLFW_KEY_DOWN) & PRESSED;
-    key_input.hold =
-        inputHandler.GetKeycodeState(GLFW_KEY_LEFT_SHIFT) & PRESSED;
-    key_input.left = inputHandler.GetKeycodeState(GLFW_KEY_LEFT) & PRESSED;
-    key_input.right = inputHandler.GetKeycodeState(GLFW_KEY_RIGHT) & PRESSED;
+    key_input.rotate_cw  = inputHandler.GetKeycodeState(GLFW_KEY_UP) & PRESSED;
+    key_input.rotate_ccw = inputHandler.GetKeycodeState(GLFW_KEY_LEFT_CONTROL) & PRESSED;
+    key_input.hard_drop  = inputHandler.GetKeycodeState(GLFW_KEY_SPACE) & PRESSED;
+    key_input.soft_drop  = inputHandler.GetKeycodeState(GLFW_KEY_DOWN) & PRESSED;
+    key_input.hold       = inputHandler.GetKeycodeState(GLFW_KEY_LEFT_SHIFT) & PRESSED;
+    key_input.left       = inputHandler.GetKeycodeState(GLFW_KEY_LEFT) & PRESSED;
+    key_input.right      = inputHandler.GetKeycodeState(GLFW_KEY_RIGHT) & PRESSED;
 
     // Retrieve the actual framebuffer size: for HiDPI monitors,
     // you might end up with a framebuffer larger than what you
@@ -242,16 +218,13 @@ void edaf80::Assignment5::run() {
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
     // Update Screen
-    if (tick(tetris_game,
-             tetris_params_t{key_input,
-                             static_cast<time_us_t>(deltaTimeUs.count())})) {
+    if (tick(tetris_game, tetris_params_t{key_input, static_cast<time_us_t>(deltaTimeUs.count())})) {
       glBindBuffer(GL_PIXEL_UNPACK_BUFFER, screen_pbo);
-      auto ptr = (uint32_t *)glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+      auto ptr = (uint32_t*)glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
       if (ptr) {
         for (int y = 0; y < 20; ++y) {
           for (int x = 0; x < 10; ++x) {
-            ptr[(FBH - (y + 3)) * FBW + (x + 11)] =
-                get_color(read_game(tetris_game, x, y));
+            ptr[(FBH - (y + 3)) * FBW + (x + 11)] = get_color(read_game(tetris_game, x, y));
           }
         }
         {
@@ -261,10 +234,8 @@ void edaf80::Assignment5::run() {
             const auto h = get_hold_height(tetris_game);
             for (int y = 0; y < 2; ++y) {
               for (int x = 0; x < 4; ++x) {
-                const int i = (FBH - (BOARD_OFFSET_Y + y)) * FBW +
-                              (x + BOARD_OFFSET_X - 5);
-                ptr[i] =
-                    (y < h && x < w) ? get_color(fb[y * w + x]) : COLOR_BLACK;
+                const int i = (FBH - (BOARD_OFFSET_Y + y)) * FBW + (x + BOARD_OFFSET_X - 5);
+                ptr[i]      = (y < h && x < w) ? get_color(fb[y * w + x]) : COLOR_BLACK;
               }
             }
           }
@@ -276,10 +247,8 @@ void edaf80::Assignment5::run() {
             const auto h = get_next_height(tetris_game, b);
             for (int y = 0; y < 2; ++y) {
               for (int x = 0; x < 4; ++x) {
-                const int i = (FBH - (BOARD_OFFSET_Y + y + b * 4)) * FBW +
-                              (x + BOARD_OFFSET_X + BOARD_WIDTH + 1);
-                ptr[i] =
-                    (y < h && x < w) ? get_color(fb[y * w + x]) : COLOR_BLACK;
+                const int i = (FBH - (BOARD_OFFSET_Y + y + b * 4)) * FBW + (x + BOARD_OFFSET_X + BOARD_WIDTH + 1);
+                ptr[i]      = (y < h && x < w) ? get_color(fb[y * w + x]) : COLOR_BLACK;
               }
             }
           }
@@ -288,26 +257,22 @@ void edaf80::Assignment5::run() {
       glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
 
       glBindTexture(GL_TEXTURE_2D, screen_texture);
-      glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, FBW, FBH, GL_RGBA,
-                      GL_UNSIGNED_BYTE, nullptr);
+      glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, FBW, FBH, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
       glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
     }
 
     if (!shader_reload_failed) {
       arcade_machine.render(mCamera.GetWorldToClipMatrix());
 
-      static float rot_angle = 0.0f;
-      const float target_angle = (static_cast<float>(key_input.left) -
-                                  static_cast<float>(key_input.right)) *
-                                 0.2f;
-      rot_angle = rot_angle + (target_angle - rot_angle) * delta_time * 20.f;
+      static float rot_angle    = 0.0f;
+      const float  target_angle = (static_cast<float>(key_input.left) - static_cast<float>(key_input.right)) * 0.4f;
+      rot_angle                 = rot_angle + (target_angle - rot_angle) * delta_time * 20.f;
 
-      const auto rot =
-          glm::rotate(glm::mat4(1.0f), rot_angle, glm::vec3(0.f, 0.f, 1.f));
-      const auto trans =
-          glm::translate(glm::mat4(1.0f), glm::vec3(-0.462f, 2.35f, 0.75f));
+      const auto rotZ  = glm::rotate(glm::mat4(1.0f), rot_angle, glm::vec3(0.f, 0.f, 1.f));
+      const auto rotX  = glm::rotate(glm::mat4(1.0f), 0.2f, glm::vec3(1.f, 0.f, 0.f));
+      const auto trans = glm::translate(glm::mat4(1.0f), glm::vec3(-0.462f, 2.35f, 0.75f));
 
-      joystick.render(mCamera.GetWorldToClipMatrix(), trans * rot);
+      joystick.render(mCamera.GetWorldToClipMatrix(), trans * rotX * rotZ);
 
       screen.render(mCamera.GetWorldToClipMatrix());
     }
@@ -328,7 +293,7 @@ int main() {
   try {
     edaf80::Assignment5 assignment5(framework.GetWindowManager());
     assignment5.run();
-  } catch (std::runtime_error const &e) {
+  } catch (std::runtime_error const& e) {
     LogError(e.what());
   }
 }
